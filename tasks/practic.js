c897d383ea0f05136fdf0a896ffe8f2b45098331
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2023-01-06 19:40:43 +0300

    add task
    add task with memoized fibonacci

--- tasks/practic.js
+++ tasks/practic.js
@@ -257,3 +257,19 @@ var hotpo = function(n){
       } 
     return counter;   
 }
+
+//Мемоизация числа фиббоначи 
+let fibonacci = (function (){
+    let memo = [0,1];
+        let result = memo[n];
+        if (typeof result !== 'number'){
+            result = fib(n-1) + fib(n-2);
+            memo[n] = result;
+        }
+        return result;
+    }
+    return fib;
+}())
+
+console.log(fibonacci(8))
+console.log(fibonacci(7))
